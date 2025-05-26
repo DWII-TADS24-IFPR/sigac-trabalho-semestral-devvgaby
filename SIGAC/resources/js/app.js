8cfeee831a3f7978ef4d2d7e619e091004f7ac0a
Gaby Lopes <developergabrielly@gmail.com>
2025-05-26 12:45:14 -0300

    Importação da biblioteca Chart.js para gráficos

--- SIGAC/resources/js/app.js
+++ SIGAC/resources/js/app.js
@@ -2,6 +2,9 @@ import "./bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import 'bootstrap'; 
 
+import Chart from 'chart.js/auto';
+window.Chart = Chart;
+
 import Alpine from "alpinejs";
 
 window.Alpine = Alpine;
