074d8d47174dc45c6e4ee736e9b76275261a764b
Gaby Lopes <developergabrielly@gmail.com>
2025-05-26 21:44:10 -0300

    Mudança do atributo nome para name na tabela Users.

--- SIGAC/app/Models/User.php
+++ SIGAC/app/Models/User.php
@@ -20,7 +20,7 @@ class User extends Authenticatable
 
     protected $table = "users";
 
-    protected $fillable = ['nome', 'email', 'password', 'role_id', 'curso_id'];
+    protected $fillable = ['name', 'email', 'password', 'role_id', 'curso_id'];
 
     public function aluno()
     {
